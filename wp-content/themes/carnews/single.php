0d3ab91390ea0119d8178ad0b7ec55900e09ecd3
Selim Reza <selimreza9920@gmail.com>
2019-09-12 02:11:45 +0600

    related post show

--- wp-content/themes/carnews/single.php
+++ wp-content/themes/carnews/single.php
@@ -13,7 +13,42 @@
                         <h2><?php the_title(); ?></h2>
                        <?php the_content(); ?>
                     </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="related_post_area">
+                                <h3>Related Posts</h3>
+                                <?php
+                                //for use in the loop, list 5 post titles related to first tag on current post
+                                $tags = wp_get_post_tags($post->ID);
+                                if ($tags) {
+                                    echo 'Related Posts';
+                                    $first_tag = $tags[0]->term_id;
+                                    $args=array(
+                                        'tag__in' => array($first_tag),
+                                        'post__not_in' => array($post->ID),
+                                        'posts_per_page'=>3,
+                                        'caller_get_posts'=>1
+                                    );
+                                    $my_query = new WP_Query($args);
+                                    if( $my_query->have_posts() ) {
+                                        while ($my_query->have_posts()) : $my_query->the_post(); ?>
+                                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                                <div class="related_post_item">
+                                                    <?php the_post_thumbnail('single-post-thumbnail'); ?>
+                                                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>                                          <?php the_excerpt(); ?>
+                                                </div>
+                                            </div>
 
+                                            <?php
+                                        endwhile;
+                                    }
+                                    wp_reset_query();
+                                }
+                                ?>
+
+                            </div>
+                        </div>
+                    </div>
                     <div class="row">
                         <div class="col-md-12">
                             <div class="comment-area">
