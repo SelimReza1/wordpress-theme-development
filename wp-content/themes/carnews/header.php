ac3d375a60df464446a2489e1c726894162d1fbe
Selim Reza <selimreza9920@gmail.com>
2019-09-12 00:33:22 +0600

    comment section dynamic

--- wp-content/themes/carnews/header.php
+++ wp-content/themes/carnews/header.php
@@ -35,6 +35,12 @@
     <script src="<?php echo get_template_directory_uri(); ?>/https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    //comments reply onlick without refresh
+    <?php
+    if(is_singular() && comments_open() && get_option('thread_comments')){
+        wp_enqueue_script('comment-reply');
+    }
+    ?>
     <?php wp_head(); ?>
 </head>
 
